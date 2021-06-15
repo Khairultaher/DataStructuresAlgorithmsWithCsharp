5d8a77c4585ebdf95e6b7fb48a448514c0de8f06
khairul <khairultaher@gmail.com>
2021-06-15 07:13:01 +0600

    2021-06-15.2

---
+++ AlgorithmExpert/Basic/Searching.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmExpert.Basic
+{
+    public static class Searching
+    {
+        // O(log(n)) time | O(n)
+        public static int BynarySearch(int[] arr, int target)
+        {
+            Array.Sort(arr);
+            int min = 0; int max = arr.Length - 1;
+            while (min <= max)
+            {
+                int middle = (min + max) / 2;
+                int match = arr[middle];
+                if (target == match)
+                {
+                    return arr[middle];
+                }
+
+                if (target > arr[middle])
+                {
+                    min = middle + 1;
+                }
+                else
+                {
+                    max = middle - 1;
+                }
+            }
+
+            return -1;
+        }
+
+    }
+}
---
+++ AlgorithmExpert/Basic/Sorting.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmExpert.Basic
+{
+    public static class Sorting
+    {
+        // O(n^2)
+        public static int[] BubbleSort(int[] arr)
+        {
+
+            int n = arr.Length - 1;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        var temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        // O(n^2)
+        public static int[] SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                var smallest = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                var temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+            return arr;
+        }
+
+        // O(n^2)
+        public static int[] InsertionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (arr[j - 1] > arr[j])
+                    {
+                        int temp = arr[j - 1];
+                        arr[j - 1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        // O(nlog(n))
+        public static List<int> MergeSort(List<int> unsorted)
+        {
+            if (unsorted.Count <= 1)
+                return unsorted;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            int middle = unsorted.Count / 2;
+            // left = unsorted.Skip(0).Take(middle).ToList();
+            for (int i = 0; i < middle; i++)  //Dividing the unsorted list
+            {
+                left.Add(unsorted[i]);
+            }
+            // left = unsorted.Skip(middle).ToList();
+            for (int i = middle; i < unsorted.Count; i++)
+            {
+                right.Add(unsorted[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            return Merge(left, right);
+        }
+
+        public static List<int> Merge(List<int> left, List<int> right)
+        {
+            List<int> result = new List<int>();
+
+            while (left.Count > 0 || right.Count > 0)
+            {
+                if (left.Count > 0 && right.Count > 0)
+                {
+                    if (left.First() <= right.First())  //Comparing First two elements to see which is smaller
+                    {
+                        result.Add(left.First());
+                        left.Remove(left.First());      //Rest of the list minus the first element
+                    }
+                    else
+                    {
+                        result.Add(right.First());
+                        right.Remove(right.First());
+                    }
+                }
+                else if (left.Count > 0)
+                {
+                    result.Add(left.First());
+                    left.Remove(left.First());
+                }
+                else if (right.Count > 0)
+                {
+                    result.Add(right.First());
+                    right.Remove(right.First());
+                }
+            }
+            return result;
+        }
+    }
+}
