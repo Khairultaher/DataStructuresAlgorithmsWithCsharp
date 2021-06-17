5cd1ee863fb724fd044a9c48fc2b35de479ae896
khairul <khairultaher@gmail.com>
2021-06-17 07:00:02 +0600

    2021-06-17.t1

---
+++ AlgorithmExpert/Easy/DepthFirstSearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmExpert.Easy
+{
+    public class Node
+    {
+        public string name;
+        public List<Node> children = new List<Node>();
+        public Node(string name)
+        {
+            this.name = name;
+        }
+        // O(v + e) time | O(v) space
+        public List<string> DepthFirstSearch(List<string> array)
+        {
+            array.Add(this.name);
+            for (int i = 0; i < children.Count; i++)
+            {
+                children[i].DepthFirstSearch(array);
+            }
+            return array;
+        }
+        public Node AddChild(string name)
+        {
+            Node child = new Node(name);
+            children.Add(child);
+            return this;
+        }
+    }
+}
