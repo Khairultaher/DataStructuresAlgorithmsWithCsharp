793a1096c489212d3ad7ccba4e6c1db964d0a8c5
Khairul Taher <khairultaher@outlook.com>
2021-07-17 23:58:39 +0600

    2021-07-17.2

---
+++ TecognizeClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using TecognizeClass.Day_1;
+
+namespace TecognizeClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ReverseAnArray.BruteForce();
+            ReverseAnArray.UsingSwap();
+            Console.ReadLine();
+        }
+    }
+}
